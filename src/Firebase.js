f5bff2b731bfaf7432ac004ff051550c7e1d5064
karanjeet <bawa31397@gmail.com>
2025-08-03 21:22:33 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,26 +2,23 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, onValue, push } from "firebase/database";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDJsuqiD5x5P4iCmran_T1XLrvwGNYrOqY",
   authDomain: "invest-e2d40.firebaseapp.com",
   projectId: "invest-e2d40",
-  storageBucket: "invest-e2d40.firebasestorage.app",
+  storageBucket: "invest-e2d40.appspot.com", 
   messagingSenderId: "510720998891",
   appId: "1:510720998891:web:0eef9da64d19f100036dae",
   measurementId: "G-BEDEKQLDJQ",
-  databaseURL: "https://invest-e2d40-default-rtdb.firebaseio.com" 
+  databaseURL: "https://invest-e2d40-default-rtdb.firebaseio.com",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-const auth = getAuth(app);
-const database = getDatabase(app);
-
-// ✅
-export { auth, database, ref, onValue, push };
+export const auth = getAuth(app);
+export const database = getDatabase(app);
+export const storage = getStorage(app); 
+export { ref, onValue, push };
